c5d2221ee2946cb47400d0a0ba0892612793bb09
Mendizabal <rusmz@Mendizabals-MacBook-Air.local>
2026-05-30 21:25:00 +0200

    added fetch, promise fn and cards shuffling fn

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,60 @@
+import { useState, useEffect } from "react";
+
 import Score from "./components/score.jsx"
 import Card from "./components/card.jsx"
 
 import './App.css'
 
-export default function App() {
+const pokemonsArr = ["charmander", "charmeleon", "charizard", "squirtle", "wartortle", "blastoise", "bulbasaur", "ivysaur", "venusaur", "zapdos", "articuno", "moltres"];
 
+const shuffleArray = (array) => {
+  const newArray = [...array];
+
+  for (let i = newArray.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+
+    [newArray[i], newArray[j]] = [newArray[j], newArray[i]];
+  }
+
+  return newArray;
+};
+
+export default function App() {
+  const [pokemons, setPokemons] = useState([]);
+
+  useEffect(() => {
+    getPokemons()
+  }, []);
+  
+  const getPokemons = async () => {
+    try {
+      const responses = await Promise.all(
+        pokemonsArr.map((pokemon) => fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`))
+      );
+      
+      const data = await Promise.all(responses.map((res) => res.json()));
+     
+      const formatted = data.map((pokemon) => ({
+        id: pokemon.id,
+        name: pokemon.name,
+        image: pokemon.sprites.front_default,
+      }));
+
+      setPokemons(shuffleArray(formatted));
+    }
+      
+    catch (error) {
+      console.error(error);
+    }
+  };
 
   return (
     <>
       <Score />
       <div className="cards-container">
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+        {pokemons.map(pokemon => (
+          <Card key={pokemon.id} pokemon={pokemon}/>
+        ))}
       </div>
     </>
   )
--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,9 +1,9 @@
 import "../style/card.css"
 
-export default function Card() {
+export default function Card({pokemon}) {
     return (
         <div className="card">
-            
+            <img src={pokemon.image} alt={pokemon.name} />
         </div>
     )
 } 
